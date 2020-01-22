bc01792bfed590cdb5c81bcda8b2bd5cfaba350a
sukim1226 <sukim1226@gmail.com>
2020-01-22 17:59:30 +0900

    natural join 함수를 insert_combination_list 함수로 수정

--- CPP_PaymentTestcase/combination.cpp
+++ CPP_PaymentTestcase/combination.cpp
@@ -45,15 +45,24 @@ void make_combination(Combination<T>& comb, vector<T*>& content, bool push, int 
 
 }
 
-void naturalJoin() {
-	vector <Combination<Product>*> prod;
-	vector <Combination<Method>*> mth;
-	vector <Combination<Discount>*> dis;
+void insert_combination_list(void) {
 
-	Combination<Product>* comb = new Combination<Product>();
-
+	Combination<Product>* product = new Combination<Product>();
 	for (int i = 0; i < productList.size(); i++) {
-		make_combination <Product> (*comb, productList, true, i, 0);
+		make_combination <Product> (*product, productList, true, i, 0);
+		productComb.push_back(product);
+	}
+
+	Combination<Method>* method = new Combination<Method>();
+	for (int i = 0; i < methodList.size(); i++) {
+		make_combination <Method>(*method, methodList, true, i, 0);
+		methodComb.push_back(method);
+	}
+
+	Combination<Discount>* discount = new Combination<Discount>();
+	for (int i = 0; i < discountList.size(); i++) {
+		make_combination <Discount>(*discount, discountList, true, i, 0);
+		discountComb.push_back(discount);
 	}
 
 }
